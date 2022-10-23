b97233227f7ce7f906f9df144222769875637412
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2022-10-23 15:06:20 +0200

    change BG & Responsive

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
 "use strict";
 
-const secondHand = document.querySelector('.second-hand');
+const secondHand = document.querySelector('.second-hand'),
+  minsHand = document.querySelector('.min-hand'),
+  hoursHand = document.querySelector('.hour-hand');
 
 const setDate = () => {
   const now = new Date();
+
   const seconds = now.getSeconds();
-  const secondsDegrees = ((seconds / 60) * 360);//detect how many degrees to rotate. 60 seconds in minute and 360 degrees max
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+
+  const mins = now.getMinutes();
+  const minsDegrees = ((mins / 60) * 360) + 90;
+
+  const hours = now.getHours();
+  const hoursDegrees = ((hours / 12) * 360) + 90;
 
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+  hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
